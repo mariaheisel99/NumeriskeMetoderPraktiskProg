63cff9fbc225c7f686d2cfba0c06d26752da84da
Maria Heisel <mariaheisel99@gmail.com>
2023-02-19 18:24:04 +0100

    lavet exercise1, men kan ikke løse 2 umiddelbart, spørg Dimitri

--- exercises/generic_list/genlist.cs
+++ exercises/generic_list/genlist.cs
@@ -7,6 +7,8 @@ public class genlist<T>{ // alt efter hvad T er, så bliver listen under en arra
 	public int size => data.Length; //property, size er lige lenght af data
 	public T this[int i] => data[i]; // indexer
 	public genlist(){ data = new T[0]; } //constructor
+
+	// Exercise1
 	public void add(T item) { /* add item to the list */
 		//realocate an array. Vi laver en nu array som har en ekstra plads og overføre hele den gamle array ind i den.
 		T[] newdata = new T[size + 1];  
@@ -14,6 +16,19 @@ public class genlist<T>{ // alt efter hvad T er, så bliver listen under en arra
 		newdata[size] = item;
 		data = newdata; // data is no a reference to the object newdata. So data is a pointer to newdata in ram, and newdata is pointed to its definition in the ram. The things in data before is lost beacuse it doesn't point to it anymore. 
 		}
+	
+	// Exercise2
+	public void remove(int i) {
+		T[] newdata = new T[size - 1];
+		int j = 0;
+		for (int k = 0;k<size; k++){
+			if (k!=1) {
+			newdata[j] = data [k];}
+			j++;
+		}
+	data = newdata;
+	}
+
 }
 
 
--- exercises/generic_list/main.cs
+++ exercises/generic_list/main.cs
@@ -5,20 +5,20 @@ using static System.Math;
 class Program {
         public static int Main() {
 	////////////////////
-	genlist<double> listd = new genlist<double>();
-	listd.add(1.0);
-	listd.add(2.0);
-	listd.add(3.0);
+	//genlist<double> listd = new genlist<double>();
+	//listd.add(1.0);
+	//listd.add(2.0);
+	//listd.add(3.0);
 
-	Func<double,double> f;
-	f = Sin;
-	f = delegate(double x){return x*x;};
-	f = (double x) => x*x;
-	double y = f(2.0);
-	for(int i=0;i<listd.size;i++){
-		double x = listd[i];
-		WriteLine($"{x} {f(x)}");
-	}
+	//Func<double,double> f;
+	//f = Sin;
+	//f = delegate(double x){return x*x;};
+	//f = (double x) => x*x;
+	//double y = f(2.0);
+	//for(int i=0;i<listd.size;i++){
+	//	double x = listd[i];
+	//	WriteLine($"{x} {f(x)}");
+	//}
 	
 	//////////////////////
         var list = new genlist<double[]>();
@@ -32,12 +32,23 @@ class Program {
                 var numbers = new double[n];
                 for( int i=0;i<n;i++) numbers[i] = double.Parse(words[i]);
                 list.add(numbers);
-                }
+		WriteLine("Before removing item 2");
+	}
         for(int i=0;i<list.size;i++){
                 var numbers = list[i];
                 foreach(var number in numbers)Write($"{number : 000e+00;-0.00e+00}");
                 WriteLine();
                 }
+////////////////
+
+	list.remove(2);
+	WriteLine("After removing item 2");
+	for(int i=0;i<list.size;i++){
+                var numbers = list[i];
+                foreach(var number in numbers)Write($"{number : 000e+00;-0.00e+00}");
+                WriteLine();		
+		
         return 0;
         }
-} 
+
+}
