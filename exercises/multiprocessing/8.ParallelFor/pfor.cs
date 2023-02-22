8800c43b8b4db054dfbf90735a421ab601c82460
Maria Heisel <mariaheisel99@gmail.com>
2023-02-22 12:51:29 +0100

    update 22.02.23

--- exercises/multiprocessing/8.ParallelFor/pfor.cs
+++ exercises/multiprocessing/8.ParallelFor/pfor.cs
@@ -6,17 +6,17 @@ using static System.Math;
 class main{
 
 public static int Main(string[] args){
-	int nterms=(int)1e8, nthreads=1;
+	int nterms=(int)1e8;
 	foreach(var arg in args)
 	{
 		var words = arg.Split(':');
 		if(words[0]=="-terms"  )nterms  =(int)float.Parse(words[1]);
-		if(words[0]=="-threads")nthreads=(int)float.Parse(words[1]);
 	}
-	WriteLine($"nterms={nterms} nthreads={nthreads}");
-
+	
 	double sum=0;
+	//WriteLine("Ordinary serial loop result:");
 	//for(int i=1;i<nterms+1;i++){sum+=1.0/i;}
+	WriteLine("harmonic sum Parallal.For loop result:");
 	Parallel.For( 1, nterms+1, delegate(int i){ sum+=1.0/i;} );
 	WriteLine($"nterms={nterms} sum={sum}");
 
