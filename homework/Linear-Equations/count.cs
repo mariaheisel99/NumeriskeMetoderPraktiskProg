39ca9f85f339dac4e042654f61c40fcb4a54d8fd
Maria Heisel <mariaheisel99@gmail.com>
2023-03-02 08:03:08 +0100

    update 02.03.23

--- homework/Linear-Equations/count.cs
+++ homework/Linear-Equations/count.cs
@@ -1,9 +1,17 @@
 using System;
-using System.Console;
+using static System.Console;
 
 class programPlot{
-
-static void Main(){
+	
+static void Main(string[] args){
+	int ndim = 1;
+	foreach(var arg in args){
+	var words = arg.Split(':');
+	if(words[0] == "-dim")ndim = (int)float.Parse(words[1]);
+	}
+	WriteLine($"{ndim}");
+	var A = matrix.random_matrix(ndim,ndim);
+	var (Q,R) = QRGS.dec(A);
 	
 }//Main 
 
