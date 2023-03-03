11092f4f28caa887543d680f62db5688106652a4
Maria Heisel <mariaheisel99@gmail.com>
2023-03-03 19:41:15 +0100

    update on plot

---
+++ homework/Linear-Equations/Nfit.cs
@@ -0,0 +1,11 @@
+using System;
+using static System.Math;
+using static System.Console;
+
+public class fit{
+public static void Main(){
+	for(double x=400;x<1000;x+=2.0){
+		WriteLine($"{x} {1.5e-8*Pow(x,3)}");
+	}
+}
+}
--- homework/Linear-Equations/count.cs
+++ homework/Linear-Equations/count.cs
@@ -9,10 +9,8 @@ static void Main(string[] args){
 	var words = arg.Split(':');
 	if(words[0] == "-dim")ndim = (int)float.Parse(words[1]);
 	}
-	WriteLine($"{ndim}");
 	var A = matrix.random_matrix(ndim,ndim);
 	var (Q,R) = QRGS.dec(A);
-	
 }//Main 
 
 }//class
