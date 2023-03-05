f234f91cde8d2b1d7adb49ba13cac146ff173590
Maria Heisel <mariaheisel99@gmail.com>
2023-03-05 15:16:47 +0100

    update 05.03.23

--- homework/Linear-Equations/Nfit.cs
+++ homework/Linear-Equations/Nfit.cs
@@ -4,7 +4,7 @@ using static System.Console;
 
 public class fit{
 public static void Main(){
-	for(double x=400;x<1000;x+=2.0){
+	for(double x=400;x<1100;x+=2.0){
 		WriteLine($"{x} {1.5e-8*Pow(x,3)}");
 	}
 }
