ce68ff6e88fa12931d13f70933efb8b0775b428a
Maria Heisel <mariaheisel99@gmail.com>
2023-04-14 11:53:40 +0200

    Finised part A

--- homework/montecarlo/main.cs
+++ homework/montecarlo/main.cs
@@ -3,20 +3,27 @@ using static System.Console;
 using static System.Math;
 
 public class main{
-	public static void Main(string[] args){
-	int N = 1;
-	N = (int)double.Parse(args[0]);
-	//enhedscirkel
-	var r = 1;
-	Func<vector, double> f = x =>{
-		if(x.norm() <=r) return 1;
-		else return 0;
-		};
-	vector a = new vector(0.0,0.0); //lower limit
-	vector b = new vector(1.0,1.0); // upper limit
+	public static void Main(){
+	int N = 50000;
+	
+	//First test of integral 
+	WriteLine("Integration test of a 2 dimensional function x*cos(y) with x[0,1] and y[0,pi/2]");
+	Func<vector, double> f = x =>x[0]*Cos(x[1]);
+	vector a = new vector(0,0); //lower limit
+	vector b = new vector(1.0,PI/2.0); // upper limit
 	(double mean, double sigma) = montecarlo_integrator.plainmc(f,a,b,N);
-	double true_val = PI/4.0;
-	WriteLine($"{N} {mean} {sigma} {Abs(mean-true_val)}");
+	double true_val = 1.0/2;
+	WriteLine($"The value is = {mean} with error {sigma} and the actual error fra true value: {Abs(mean-true_val)}");		
+	Write("\n\n");
+	WriteLine("Integration of a 3 dimensional function (1-cos(x)*cos(y)*cos(z))^-1*pi^-3 with x[0,pi] and y[0,pi] and z[0,pi]");
+	Func<vector, double> g = x =>1.0/(1-Cos(x[0])*Cos(x[1])*Cos(x[2]))*Pow(PI,-3);
+	vector c = new vector(0,0,0); //lower limit
+	vector d = new vector(PI,PI,PI); // upper limit
+	(double mean2, double sigma2) = montecarlo_integrator.plainmc(g,c,d,N);
+	double true_val2= Pow(sfuns.gamma(1.0/4),4)/(4*Pow(PI,3));
+	Error.Write($"value {true_val2}");
+	WriteLine($"The value is = {mean2} with error {sigma2} and the actual error fra true value: {Abs(mean2-true_val2)}");		
+	
 	
 		
 	}//Main
---
+++ homework/montecarlo/mainEnhedscirkel.cs
@@ -0,0 +1,25 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+public class main{
+	public static void Main(string[] args){
+	int N = 1;
+	N = (int)double.Parse(args[0]);
+	
+	//enhedscirkel test
+	var r = 1;
+	Func<vector, double> f = x =>{
+		if(x.norm() <=r) return 1; //norm of vector x containg x and y in the total area is less than 1 inside unit circle
+		else return 0;
+		};
+	vector a = new vector(-1.0,-1.0); //lower limit
+	vector b = new vector(1.0,1.0); // upper limit
+	(double mean, double sigma) = montecarlo_integrator.plainmc(f,a,b,N);
+	double true_val = PI;
+	double fit = 1.0/Sqrt(N);
+	WriteLine($"{N} {mean} {sigma} {Abs(mean-true_val)} {fit}");		
+	
+		
+	}//Main
+}//class
