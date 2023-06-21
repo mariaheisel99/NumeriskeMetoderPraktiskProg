b2872038118a1037a00e2c483137fc20d8edcda7
Maria Heisel <mariaheisel99@gmail.com>
2023-06-21 11:57:11 +0200

    B in ann finished

--- homework/ArtificialNeutralNetworks/ann.cs
+++ homework/ArtificialNeutralNetworks/ann.cs
@@ -12,14 +12,44 @@ public class ann{
 		p = new vector(nparams);	
 
 	}
+	public double a(int i) {return p[i];}
+	public double b(int i) {return p[i+n];}
+	public double w(int i) {return p[i+2*n];}
 
+	public double df_gaussianwavlet(double x){
+		double sum = 0;
+		Func<double,double> df = z => Exp(-z*z)*(1-2*z*z);
+		for(int i = 0; i<n;i++){
+			sum +=w(i)*df((x-a(i))/b(i))/b(i);
+		} return sum;
+	}//df_gaussianwavlet 
+
+
+	public double ddf_gaussianwavlet(double x){
+		double sum = 0;
+		Func<double,double> ddf = z => Exp(-z*z)*(-6*z+4*z*z*z);
+		for(int i = 0; i<n;i++){
+			sum +=w(i)*ddf((x-a(i))/b(i))/(b(i)*b(i));
+		} return sum;
+	}//df_gaussianwavlet 
+
+
+	public double F_gaussianwavlet(double x,double start){
+		double sum = 0;
+		Func<double,double> F = z => Exp(-z*z)*(-1.0/2);
+		for(int i = 0; i<n;i++){
+			sum +=w(i)*F((x-a(i))/b(i))*b(i);
+			sum -=w(i)*F((start-a(i))/b(i))*b(i);
+		} return sum;
+	}//df_gaussianwavlet 
+	
 	public double response(double x){/* return the response of the network to the input signal x*/
 		//Respons Fp(x) = sum_i f((x-ai)/bi)*wi
 		double F = 0;
 		for(int i = 0; i < n; i++){
-			double ai = p[i]; 
-			double bi = p[i+n];
-			double wi = p[i+2*n];
+			double ai = a(i); 
+			double bi = b(i);
+			double wi = w(i);
 			F += f((x-ai)/bi)*wi;}
 		return F;}
 
@@ -29,8 +59,8 @@ public class ann{
 		
 		//determine ai parameteres ditrubted evenly over by inizialing p vecttor with value of the network paramteres. bi and wi are set to default 1
 		for(int i = 0; i<p.size;i++){
-			if(i<n) {p[i]=xmin + (xmax-xmin)*i/(n-1);
-				Error.WriteLine($"x = {p[i]}");}
+			if(i<n) {p[i]=xmin + (xmax-xmin)*i/(n-1);}
+			
 			else p[i] = 1;
 		}
 		Error.WriteLine("Done points distributed");
@@ -50,4 +80,5 @@ public class ann{
 		Error.WriteLine("Done minimum");
 		var result = v;
 		}
+
 }//ann class
--- homework/ArtificialNeutralNetworks/main.cs
+++ homework/ArtificialNeutralNetworks/main.cs
@@ -9,7 +9,6 @@ public class main{
 		WriteLine("------- Part A --------");
 		WriteLine("Test of ann.cs are performed with training points for g(x)=Cos(5*x-1)*exp(-x^2)");
 		WriteLine("The minimization of the cost function is performed with quasi-newton minimization");
-		Func<double,double> Gw = x => x*Exp(-x*x); //Gaussian Wavelet
 		Func<double,double> g = x => Cos(5*x-1)*Exp(-x*x);// test function g(x)
 		//constructing x- and y-points
 		WriteLine("Construting x and y trainingpoitns on the interval of -1 to 1 wiht 20 points. The Network is set up with 6 neurons");
@@ -34,6 +33,33 @@ public class main{
 			Networkpoints.WriteLine($"{j} {network.response(j)}");		
 		}//forloop
 		Networkpoints.Close();
+
+
+		WriteLine("---------- Part B ----------");
+		WriteLine("Performing ANN on a Gaussian Wavlet f(x)=x*exp(-x^2)");
+		WriteLine("Return also first and second derivatives and the anti-dericative of the approximant to the Gaussain Wavlet");
+		
+		Func<double,double> Gw = x => x*Exp(-x*x); //Gaussian Wavelet
+		WriteLine("Construting x and y trainingpoitns on the interval of -1 to 1 wiht 20 points. The Network is set up with 5 neurons");
+		var outpointsB = new StreamWriter("TrainPointsB.txt");
+		vector x_s = new vector(m), y_s = new vector(m);
+		for(int i = 0; i< m;i++){ //equal distributed uniform points
+			x_s[i] = a+(b-a)*i/(m-1);
+			y_s[i] = Gw(x_s[i]);
+			outpointsB.WriteLine($"{x_s[i]} {y_s[i]}");}
+		outpointsB.Close();
+		var NetworkpointsB = new StreamWriter("NetworkPointsB.txt");
+		var derivatives = new StreamWriter("DerivativesB.txt");
+		int N = 10;
+		ann network2 = new ann(N,Gw);
+		network2.train(x_s,y_s);
+		for(double j = a; j<b; j+=1.0/64) {
+			NetworkpointsB.WriteLine($"{j} {network2.response(j)}");
+			derivatives.WriteLine($" {j} {network2.df_gaussianwavlet(j)} {network2.ddf_gaussianwavlet(j)} {network2.F_gaussianwavlet(j,-1)}");}
+		NetworkpointsB.Close();
+		derivatives.Close();
+
+		
 	}//Main
 
 }//main class
