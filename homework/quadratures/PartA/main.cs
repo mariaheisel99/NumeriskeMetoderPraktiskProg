4c55c9620b3aa3adfe6d442b7abf5f863697d047
Maria Heisel <mariaheisel99@gmail.com>
2023-06-22 15:47:56 +0200

    A,B finished

--- homework/quadratures/PartA/main.cs
+++ homework/quadratures/PartA/main.cs
@@ -13,14 +13,14 @@ public static void Main(){
 	var result1 = integrate.integrator(f,0,1);
 	var exact1 = 2.0/3;	
 	WriteLine($"int[0;1] sqrt(x) = {result1}");
-	WriteLine($"Test result of (exact = {exact1}): {complex.approx(exact1,result1,1e-6,1e-6)}");
+	WriteLine($"Test result with error 1e-3 acceptance of (exact = {exact1}): {complex.approx(exact1,result1,1e-3,1e-3)}");
 
 	WriteLine("");
 	f = delegate(double x){return 1.0/Sqrt(x);};
 	var result2 =  integrate.integrator(f,0,1,1e-6,0);	
 	var exact2 = 2.0;
 	WriteLine($"int[0;1] 1/sqrt(x) = {result2}");
-	WriteLine($"Test result of (exact = {exact2}): {complex.approx(exact2,result2,1e-6,1e-6)}");
+	WriteLine($"Test result with error 1e-6 acceptance of (exact = {exact2}): {complex.approx(exact2,result2,1e-6,1e-6)}");
 
 	
 	WriteLine("");
@@ -28,14 +28,14 @@ public static void Main(){
 	var result3 =  integrate.integrator(f,0,1,1e-6,0);	
 	var exact3 = PI;
 	WriteLine($"int[0;1] 4*sqrt(1-x^2) = {result3}");
-	WriteLine($"Test result of (exact = {exact3}): {complex.approx(exact3,result3,1e-6,1e-6)}");
+	WriteLine($"Test result with error 1e-6 acceptance of (exact = {exact3}): {complex.approx(exact3,result3,1e-6,1e-6)}");
 
 	WriteLine("");
 	f = delegate(double x){return Log(x)/Sqrt(x);};
 	var result4 =  integrate.integrator(f,0,1,1e-6,0);	
 	var exact4 = -4.0;
 	WriteLine($"int[0;1] ln(x)/sqrt(x) = {result4}");
-	WriteLine($"Test result of (exact = {exact4}): {complex.approx(exact4,result4,1e-6,1e-6)}");
+	WriteLine($"Test result with error 1e-6 acceptance of (exact = {exact4}): {complex.approx(exact4,result4,1e-6,1e-6)}");
 	
 	
 
