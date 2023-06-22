ae445b0dd97066fdce5321f8d6dc6937fbfd495f
Maria Heisel <mariaheisel99@gmail.com>
2023-06-22 14:34:22 +0200

    Not finished

--- homework/eigenvalues/mainPartA.cs
+++ homework/eigenvalues/mainPartA.cs
@@ -6,18 +6,18 @@ public class main{
 	public static void Main(){
 
 	WriteLine("Part A:");
-	WriteLine("Genereate random 3x3 matrix A");
-	var A = matrix.random_matrix(3,3);
+	WriteLine("Genereate symmetric random 3x3 matrix A");
+	var A = matrix.random_symmetric_matrix(3);
 	var transposeA = A.T;
-	transposeA.print("AT");
 	A.print("A=");	
+	transposeA.print("A.T = ");
 	
-	WriteLine("Perform eigenvalue-decomposition with jacobi algoritme");
+	WriteLine("* Perform eigenvalue-decomposition with jacobi algoritme");
 	var (D1,V1) = jacobi.cyclic(A);
 	D1.print("D=");
 	V1.print("V=");
 
-	WriteLine("Udfører tests");
+	WriteLine("* Performance test");
 	var test1 = V1.T*A*V1;
 	var test2 = V1*D1*V1.T;
 	var test3 = V1.T*V1;
@@ -27,9 +27,13 @@ public class main{
 	WriteLine("");
 	WriteLine($"V*D*V^T=A ? => {test2.approx(A)}");
 	WriteLine("");
+	matrix I = new matrix(3,3);
+	I.set_identity();
 	test3.print("V^T*V =");
+	WriteLine($"V^T*V=I ? => {test3.approx(I)}");
 	WriteLine("");
 	test4.print("V*V^T =");
+	WriteLine($"V*V^T=I ? => {test2.approx(I)}");
 		
 	}//Main
 }//class main
